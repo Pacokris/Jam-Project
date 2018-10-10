7b4e4948d7cc8cdd613799811c40dd0884e584f5
Florent Perticoz <perticoz@hotmail.com>
2018-10-10 10:57:14 +0200

    commit nouvelle version de base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import { NavComponent } from './nav/nav.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
+import { ResultComponent } from './result/result.component';
+import { RhrnComponent } from './rhrn/rhrn.component';
+import { ListComponent } from './list/list.component';
+import { MapComponent } from './map/map.component';
+import { EventComponent } from './event/event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SearchComponent,
+    HomeComponent,
+    ResultComponent,
+    RhrnComponent,
+    ListComponent,
+    MapComponent,
+    EventComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_KEY'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
